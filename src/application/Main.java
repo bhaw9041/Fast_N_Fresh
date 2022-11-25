0dbc33f5bb4c6cadc50b61e8165400b76ee48df9
Bhawna Singh <singh.bhaw@northeastern.edu>
2022-11-25 12:29:43 -0500

    Catalog UI

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,23 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			System.out.println();
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/view/Catalog.fxml"));
+		Scene scene = new Scene(root);
+	
+		primaryStage.setTitle("Catalog Page");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
