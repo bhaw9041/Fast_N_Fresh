a28f583384fd635e304be4af9937d7b8058d3f45
maheswarpalakurthy <98120682+maheswarpalakurthy@users.noreply.github.com>
2022-11-30 14:53:11 -0500

    Database connection established

---
+++ src/model/DatabaseConnector.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.sql.*;
+
+public class DatabaseConnector {
+	
+	private static Connection databaseConnection = null;
+	
+	private DatabaseConnector(){
+	}
+	
+	public static Connection getInstance() throws Exception {
+		if (databaseConnection == null) {
+			String url = "jdbc:mysql://localhost:3306/FastNFresh";
+			String username = "root";
+			String password = "Vigilant@22";
+			Class.forName("com.mysql.jdbc.Driver");
+			databaseConnection = DriverManager.getConnection(url, username, password);
+			return databaseConnection;
+		} else {
+			return databaseConnection;
+		}
+		
+	}
+	
+	
+}
