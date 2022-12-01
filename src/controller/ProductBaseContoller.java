1aa6f01920ae6cc527e3c62c0f4dcf3da98fdfdf
maheswarpalakurthy <98120682+maheswarpalakurthy@users.noreply.github.com>
2022-11-30 19:44:59 -0500

    Inventory update complete

--- src/controller/ProductBaseContoller.java
+++ src/controller/ProductBaseContoller.java
@@ -1,7 +1,12 @@
 package controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import model.Cart;
+import model.Product;
 
 public abstract class ProductBaseContoller {
 		public static Cart cart = new Cart();
+		public static Map<String, Product> inventoryItems = new HashMap<String, Product>();
 }
